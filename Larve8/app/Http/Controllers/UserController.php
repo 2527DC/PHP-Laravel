5451f2e4feeb6d69441622c40917d341c08c3e5b
chethan <chethan962089@gmail.com>
2024-10-09 15:51:07 +0530

    ets project which is of the templet

--- Larve8/app/Http/Controllers/UserController.php
+++ Larve8/app/Http/Controllers/UserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Rules\Uppercase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class UserController extends Controller
 {
@@ -22,9 +23,46 @@ class UserController extends Controller
         ], 404); // You can change the status code as needed
     }
 
-  
-    public  function  chckValidation(){
 
-        
+    public  function  chckValidation(Request $request)
+    {
+
+        $input = $request->all();
+
+        // $validate = Validator::make($input, ['username' => 'required']);
+
+        $validate = Validator::make($input, [
+            'username' => ['required', new Uppercase()], // Validate that 'username' is required and uppercase
+        ]);
+
+        if ($validate->fails()) {
+
+            return response()->json(['error' => $validate->errors()],422);
+        } 
+
+        return response()->json(['message' => 'Username is valid!']);
     }
+
+
+    // public function chckValidation(Request $request)
+    // {
+    //     // Get all input data
+    //     $input = $request->all();
+
+    //     // Create a validator instance
+    //     $validator = Validator::make($input, [
+    //         'username' => 'required|string|max:255', // Define your validation rules here
+    //     ]);
+
+    //     // Check if validation fails
+    //     if ($validator->fails()) {
+    //         return response()->json([
+    //             'errors' => $validator->errors(), // Return the error messages
+    //         ], 422); // 422 Unprocessable Entity
+    //     }
+
+    //     // If validation passes, continue with your logic
+    //     // For example, you can save the data or return a success response
+    //     return response()->json(['message' => 'Username is valid!']);
+    // }
 }
--- Larve8/routes/api.php
+++ Larve8/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+
 
 Route::get('/message', 'App\Http\Controllers\UserController@getmessage')->name('message');
 
+
+
+Route::post('validate','App\Http\Controllers\UserController@chckValidation');
